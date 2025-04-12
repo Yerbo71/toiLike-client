261458671f10fe4ec906fa1cc3be0bb927470c82
Aijin <115892544+Yerbo71@users.noreply.github.com>
2025-04-12 14:11:33 +0500

    feat():update types

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { Image, ScrollView } from 'react-native';
 import HomeSearchBar from '@/src/pages/home/components/homeSearchBar';
 import HomeCarousel from '@/src/pages/home/components/homeCarousel';
 import HomeEventBlock from '@/src/pages/home/components/homeEventBlock';
